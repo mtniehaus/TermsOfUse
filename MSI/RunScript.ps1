5a9fdc358a3530a9829fadbfa95274fd62668e90
Michael Niehaus <niehaus@live.com>
2022-09-15 11:43:42 +0200

    Updated PowerShell script to look for "termsofuse"

--- MSI/RunScript.ps1
+++ MSI/RunScript.ps1
@@ -3,7 +3,7 @@ Start-Transcript -Path "C:\Windows\AutopilotTOU.log" -Force
 
 # Get the saved local storage from the TOU page
 Push-Location "C:\Users\defaultuser0\AppData\Local\Packages\Microsoft.Windows.CloudExperienceHost_cw5n1h2txyewy\AC\Microsoft\Internet Explorer\DOMStore"
-$localStorage = Get-ChildItem -Path babymdm*.xml -Recurse -Force
+$localStorage = Get-ChildItem -Path termsofuse*.xml -Recurse -Force
 [xml] $vars = Get-Content -LiteralPath $localStorage
 
 # Take action based on what it contains
