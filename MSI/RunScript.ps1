c900415ea0bd6d0fba2be4e2f44889293a3a384e
Michael Niehaus <niehaus@live.com>
2022-09-15 21:06:09 +0200

    Update script to write oobe.xml

--- MSI/RunScript.ps1
+++ MSI/RunScript.ps1
@@ -1,5 +1,5 @@
 # Save a transcript to prove we ran
-Start-Transcript -Path "C:\Windows\AutopilotTOU.log" -Force
+Start-Transcript -Path "C:\Windows\AutopilotTOU.log" -Append -Force
 
 # Get the saved local storage from the TOU page
 $path = "C:\Users\defaultuser0\AppData\Local\Packages\Microsoft.Windows.CloudExperienceHost_cw5n1h2txyewy\AC\Microsoft\Internet Explorer\DOMStore"
@@ -8,9 +8,23 @@ Push-Location $path
 $localStorage = Get-ChildItem -Path termsofuse*.xml -Recurse -Force
 Write-Host "Local storage path: $localStorage"
 [xml] $vars = Get-Content -LiteralPath $localStorage
+$timeZoneId = $vars.root.item.value
 
-# Take action based on what it contains
-Write-Host "Setting time zone to $($vars.root.item.value)"
-Set-TimeZone -Id $vars.root.item.value
+# Set the time zone directly
+Write-Host "Setting time zone to $timeZoneId"
+Set-TimeZone -Id $timeZoneId
+
+# In case OOBE overwrites the time zone (seems timing-dependent), write the same value to oobe.xml so msoobe.exe will set the right thing
+Write-Host "Writing C:\Windows\System32\Oobe\Info\oobe.xml with the time zone ID $timeZoneId"
+$oobeXml = @"
+<FirstExperience>
+  <oobe>
+    <defaults>
+      <timezone>$timeZoneId</timezone>
+    </defaults>
+  </oobe>
+</FirstExperience>
+"@
+$oobeXml | Out-File "C:\Windows\System32\Oobe\Info\oobe.xml" -Force -Encoding utf8
 
 Stop-Transcript
